0922e8c0d901f03f666d7169a4b95a46bceacb93
hvoncannon <hayden.voncannon@gmail.com>
2018-05-06 15:38:12 -0700

    added guesses remaining and bug fixing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,15 +3,20 @@ var words = ['segal', 'norris', 'stallone', 'zidar', 'chan', 'statham'];
 var word = words[Math.floor(Math.random() * words.length)];
 var wordArray = [];
 var guessArray = [];
+var guessedLetters = [];
 var remainingLetters = word.length;
 var remainingGuesses = word.length + 5;
 var guessDisplay = document.getElementById("guessedDiv");
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var display = document.getElementById("wordDiv");
+var remainingdisplay = document.getElementById("remainingDiv");
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 
 //creates answer array
 function getWord() {
     word = words[Math.floor(Math.random() * words.length)];
+    wordArray = [];
+    guessedLetters = [];
     remainingLetters = word.length;
     remainingGuesses = word.length + 5;
     for (var i = 0; i < word.length; i++) {
@@ -24,6 +29,7 @@ function winCheck() {
     if (remainingLetters <= 0) {
         display.innerHTML = wordArray.join("");
         guessDisplay.innerHTML = "You Win!"
+        remainingdisplay.innerHTML = '<button onclick="gameStart()">Play Again</button>';
     }
 }
 
@@ -32,6 +38,7 @@ function loseCheck() {
     if (remainingGuesses <= 0) {
         display.innerHTML = "You Lose"
         guessDisplay.innerHTML = '<button onclick="gameStart()">Play Again</button>'
+        remainingdisplay.innerHTML = "";
     }
 }
 
@@ -39,7 +46,7 @@ function loseCheck() {
 function gameStart() {
 getWord();
 guessArray = [];
-
+remainingdisplay.innerHTML = "Remaining Guesses: " + remainingGuesses;
 display.innerHTML = wordArray.join(" ");
 guessDisplay.innerHTML = guessArray
 }
@@ -53,21 +60,28 @@ document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
 
     if (letters.indexOf(userGuess) != -1) {
-//swaps out dash for letter on correct guess
-        if (word.indexOf(userGuess) != -1) {
+//swaps out dash for letter on correct guess that has not already been guessed
+        if (word.indexOf(userGuess) != -1 && guessedLetters.indexOf(userGuess) === -1) {
             for (var j = 0; j < word.length; j++) {
                 if (word[j] === userGuess) {
                     wordArray[j] = userGuess;
-                    remainingLetters--;
+                    guessedLetters.push(userGuess);
                     display.innerHTML = wordArray.join(" ");
+                    remainingLetters--;
                 }
             }
+        }
+
+//if already guessed, avoids decrementing remaining letters
+        else if (guessedLetters.indexOf(userGuess) != -1) {
+
         }
 //displays wrong guesses
         else {
             guessArray.push(userGuess)
             remainingGuesses--;
             guessDisplay.textContent = guessArray.join(" ");
+            remainingdisplay.innerHTML = "Remaining Guesses: " + remainingGuesses;
         }
     }
 
